d04b85e8e1c9b46d3c64d977c4b0c1024ad98a3c
Dénes Fehér <denes.m.feher@gmail.com>
2022-05-18 13:09:28 +0200

    Create scenario read mixed

--- test/resources/cucumber/ReadTest.feature
+++ test/resources/cucumber/ReadTest.feature
@@ -1,10 +1,21 @@
 Feature: ReadTest
   Test if a given rdfs graph has all the necessary  readable.
 
-  Scenario: Result is correct
-    Given I have a graph
+  Scenario: Read primitives
+    Given I have a graph from '/data/primitives.nt'
     And I have a mapper
-    When I read from the graph
-    Then I should get
+    When I read an instance of ClassWithPrimitives from the graph
+    Then I should get an instance of ClassWithPrimitives
       | string    | int | uri     | float | double | char |
       | str value | 8   | urn:any | 4.5f  | 20.22  | o    |
+
+  Scenario: Read mixed
+    Given I have a graph from '/data/mixed.nt'
+    And I have a mapper
+    And there is a child
+      | string    | int | uri     | float | double | char |
+      | str value | 8   | urn:any | 4.5f  | 20.22  | o    |
+    When I read an instance of ClassWithMixed from the graph
+    Then I should get an instance of ClassWithMixed
+      | string           |
+      | class with mixed |
--- test/src/cucumber/ReadStepDefinitions.java
+++ test/src/cucumber/ReadStepDefinitions.java
@@ -7,9 +7,11 @@ import java.util.Map;
 
 import com.complexible.common.openrdf.model.ModelIO;
 import com.complexible.pinto.RDFMapper;
+import com.complexible.pinto.RDFMapperTests.ClassWithMixed;
 import com.complexible.pinto.RDFMapperTests.ClassWithPrimitives;
 
 import org.openrdf.model.Model;
+import org.openrdf.model.impl.SimpleValueFactory;
 
 import io.cucumber.java.DataTableType;
 import io.cucumber.java.en.And;
@@ -22,7 +24,9 @@ import static org.junit.Assert.assertEquals;
 public class ReadStepDefinitions {
     private Model graph;
     private RDFMapper mapper;
-    private ClassWithPrimitives result;
+    private ClassWithPrimitives child;
+    private ClassWithPrimitives resultClassWithPrimitives;
+    private ClassWithMixed resultClassWithMixed;
 
     @DataTableType
     public ClassWithPrimitives classWithPrimitives(Map<String, String> entry) {
@@ -36,9 +40,17 @@ public class ReadStepDefinitions {
         return classWithPrimitives;
     }
 
-    @Given("I have a graph")
-    public void i_have_a_graph() throws Exception {
-        graph = ModelIO.read(new File(getClass().getResource("/data/primitives.nt").toURI()).toPath());
+    @DataTableType
+    public ClassWithMixed classWithMixed(Map<String, String> entry) {
+        ClassWithMixed classWithMixed = new ClassWithMixed();
+        classWithMixed.setChild(child);
+        classWithMixed.setString(entry.get("string"));
+        return classWithMixed;
+    }
+
+    @Given("I have a graph from {string}")
+    public void i_have_a_graph_from(String file) throws Exception {
+        graph = ModelIO.read(new File(getClass().getResource(file).toURI()).toPath());
     }
 
     @Given("I have a mapper")
@@ -46,16 +58,40 @@ public class ReadStepDefinitions {
         mapper = RDFMapper.create();
     }
 
-    @And("I read from the graph")
-    public void result_made_from_graph() {
-        result = mapper.readValue(graph, ClassWithPrimitives.class);
+    @Given("there is a child")
+    public void there_is_a_child(List<ClassWithPrimitives> expectedResultList) {
+        assertEquals(1, expectedResultList.size());
+        child = expectedResultList.get(0);
     }
 
-    @Then("I should get")
-    public void i_should_get(List<ClassWithPrimitives> expectedResultList) {
+    @When("I read an instance of ClassWithPrimitives from the graph")
+    public void i_read_an_instance_of_ClassWithPrimitives_from_the_graph() {
+        resultClassWithPrimitives = mapper.readValue(graph, ClassWithPrimitives.class);
+    }
+
+    @When("I read an instance of ClassWithMixed from the graph")
+    public void i_read_an_instance_of_ClassWithMixed_from_the_graph() {
+        resultClassWithMixed = mapper.readValue(
+            graph,
+            ClassWithMixed.class,
+            SimpleValueFactory.getInstance()
+                .createIRI("tag:complexible:pinto:45ad04336c95c0be6bba90e4b663da4d")
+        );
+    }
+
+    @Then("I should get an instance of ClassWithPrimitives")
+    public void i_should_get_an_instance_of_ClassWithPrimitives(List<ClassWithPrimitives> expectedResultList) {
         assertEquals(1, expectedResultList.size());
         ClassWithPrimitives expectedResult = expectedResultList.get(0);
 
-        assertEquals(expectedResult, result);
+        assertEquals(expectedResult, resultClassWithPrimitives);
+    }
+
+    @Then("I should get an instance of ClassWithMixed")
+    public void i_should_get_an_instance_of_ClassWithMixed(List<ClassWithMixed> expectedResultList) {
+        assertEquals(1, expectedResultList.size());
+        ClassWithMixed expectedResult = expectedResultList.get(0);
+
+        assertEquals(expectedResult, resultClassWithMixed);
     }
 }
