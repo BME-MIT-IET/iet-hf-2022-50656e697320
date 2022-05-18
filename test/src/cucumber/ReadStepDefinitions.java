865f311f98bc552eb70c434c0ff419e0a92de214
Dénes Fehér <denes.m.feher@gmail.com>
2022-05-18 20:14:42 +0200

    Add read test scenario

--- test/src/cucumber/ReadStepDefinitions.java
+++ test/src/cucumber/ReadStepDefinitions.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.complexible.common.openrdf.model.ModelIO;
 import com.complexible.pinto.RDFMapper;
 import com.complexible.pinto.RDFMapperTests.ClassWithMixed;
+import com.complexible.pinto.RDFMapperTests.ClassWithObjectList;
 import com.complexible.pinto.RDFMapperTests.ClassWithPrimitives;
 
 import org.openrdf.model.Model;
@@ -21,6 +22,8 @@ public class ReadStepDefinitions {
     private RDFMapper mapper;
     private ClassWithPrimitives resultClassWithPrimitives;
     private ClassWithMixed resultClassWithMixed;
+    private ClassWithObjectList classWithObjectList1;
+    private ClassWithObjectList classWithObjectList2;
 
     @Given("I have a graph from {string}")
     public void i_have_a_graph_from(String file) throws Exception {
@@ -46,6 +49,22 @@ public class ReadStepDefinitions {
                         .createIRI("tag:complexible:pinto:45ad04336c95c0be6bba90e4b663da4d"));
     }
 
+    @When("I read an instance of ClassWithObjectList from the graph")
+    public void i_read_an_instance_of_ClassWithObjectList_from_the_graph() {
+        classWithObjectList1 = mapper.readValue(
+                graph, ClassWithObjectList.class,
+                SimpleValueFactory.getInstance()
+                        .createIRI("tag:complexible:pinto:881b2f11232944aeda9ba543e030dcfc"));
+    }
+
+    @When("I read an other instance of ClassWithObjectList from the graph")
+    public void i_read_an_other_instance_of_ClassWithObjectList_from_the_graph() {
+        classWithObjectList2 = mapper.readValue(
+                graph, ClassWithObjectList.class,
+                SimpleValueFactory.getInstance()
+                        .createIRI("tag:complexible:pinto:881b2f11232944aeda9ba543e030dcfc"));
+    }
+
     @Then("I should get an instance of ClassWithPrimitives")
     public void i_should_get_an_instance_of_ClassWithPrimitives(List<ClassWithPrimitives> expectedResultList) {
         assertEquals(1, expectedResultList.size());
@@ -60,5 +79,10 @@ public class ReadStepDefinitions {
         ClassWithMixed expectedResult = expectedResultList.get(0);
 
         assertEquals(expectedResult, resultClassWithMixed);
+    }
+
+    @Then("I should get two equal instance of ClassWithObjectList")
+    public void i_should_get_two_equal_instance_of_ClassWithObjectList() {
+        assertEquals(classWithObjectList1, classWithObjectList2);
     }
 }
